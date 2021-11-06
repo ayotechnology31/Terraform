73466fa7bb9ddd6a337b3d620d4a7d92c783a2ee
ayotechnology31 <ayotechnology31@gmail.com>
2021-11-06 10:22:05 -0500

    Add files via upload

---
+++ TerraformPOC/sqs_policy/main.tf
@@ -0,0 +1,24 @@
+resource "aws_sqs_queue_policy" "test" {
+  queue_url = "https://sqs.us-east-1.amazonaws.com/762158062152/terraform-example-queue"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "${aws_sqs_queue.q.arn}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${aws_sns_topic.example.arn}"
+        }
+      }
+    }
+  ]
+}
+POLICY
+}
